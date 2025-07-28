ffd530896650ef174b65448798b5f3080452ad10
Leonardo <129320373+leonardo-dos-santos@users.noreply.github.com>
2025-07-28 16:02:40 -0300

    Multiple components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,17 +1,38 @@
-const App = () => {
-  const hoje = new Date()
-  const a = 10
-  const b = 20
-  console.log(hoje, a+b)
+// const App = () => {
+//   const hoje = new Date()
+//   const a = 10
+//   const b = 20
+//   console.log(hoje, a+b)
 
+//   return (
+//     <div>
+//       <p>Olá, mundo! Hoje é {hoje.toString()}</p>
+//       <p>
+//         {a} mais {b} é {a + b}
+//       </p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// -----Múltiplos componentes-----
+
+const Hello = () => {
   return (
     <div>
-      <p>Olá, mundo! Hoje é {hoje.toString()}</p>
-      <p>
-        {a} mais {b} é {a + b}
-      </p>
+      <p>Olá, mundo!</p>
     </div>
   )
 }
 
-export default App
+const App = () => {
+  return (
+    <div>
+      <h1>Olá a todos!</h1>
+      <Hello />
+    </div>
+  )
+}
+
+export default App;
