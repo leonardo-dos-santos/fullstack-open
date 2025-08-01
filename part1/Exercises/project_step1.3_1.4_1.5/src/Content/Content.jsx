3f6c235c3d306a012e5d31913ed814fb91a33ccd
Leonardo <129320373+leonardo-dos-santos@users.noreply.github.com>
2025-08-01 20:59:53 -0300

    Putting the versions 1.3, 1.4, 1.5 in the 'Content' component, that was the only component that had to be changed to the main program work

--- part1/Exercises/project_step1.3_1.4_1.5/src/Content/Content.jsx
+++ part1/Exercises/project_step1.3_1.4_1.5/src/Content/Content.jsx
@@ -1,3 +1,31 @@
+// ----- Part 1.3 -----
+
+// import Part from "./Part";
+
+// const Content = (props) => {
+//   return (
+//     <>
+//       <Part name={props.part1.name} exercises={props.part1.exercises} />
+//       <Part name={props.part2.name} exercises={props.part2.exercises} />
+//       <Part name={props.part3.name} exercises={props.part3.exercises} />
+//     </>
+//   );
+// };
+
+// ----- Part 1.4 -----
+
+// import Part from "./Part";
+
+// const Content = (props) => {
+//   return (
+//     <>
+//       <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+//       <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+//       <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+//     </>
+//   );
+// };
+
 import Part from "./Part"
 
 const Content = (props) => {
