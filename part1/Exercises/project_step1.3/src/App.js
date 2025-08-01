89ce6e35bf6384b1fb7a692eec03e79986896924
Leonardo <129320373+leonardo-dos-santos@users.noreply.github.com>
2025-08-01 20:17:41 -0300

    Refactoring the 1.1_1.2 exercises, in the 1.3 exercise

--- part1/Exercises/project_step1.3/src/App.js
+++ part1/Exercises/project_step1.3/src/App.js
@@ -1,7 +1,29 @@
-function App() {
-  return (
-    <></>
-  );
-}
+import Header from "./Header/Header";
+import Content from "./Content/Content";
+import Total from "./Total/Total";
 
-export default App;
+const App = () => {
+  const course = 'Desenvolvimento de aplicação Half Stack'
+  const part1 = {
+    name: 'Fundamentos da biblioteca React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Usando props para passar dados',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'Estado de um componente',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part1={part1} part2={part2} part3={part3}/>
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+    </div>
+  )
+
+}
+export default App 
