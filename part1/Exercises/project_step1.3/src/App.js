f193d8d6b1b315abf73e208edb86c77afe0ab5ba
Leonardo <129320373+leonardo-dos-santos@users.noreply.github.com>
2025-08-01 20:36:42 -0300

    Exercise 1.4, this time using Arrays intead of Objects

--- part1/Exercises/project_step1.3/src/App.js
+++ part1/Exercises/project_step1.3/src/App.js
@@ -1,29 +1,64 @@
+// import Header from "./Header/Header";
+// import Content from "./Content/Content";
+// import Total from "./Total/Total";
+
+// const App = () => {
+//   const course = 'Desenvolvimento de aplicação Half Stack'
+//   const part1 = {
+//     name: 'Fundamentos da biblioteca React',
+//     exercises: 10
+//   }
+//   const part2 = {
+//     name: 'Usando props para passar dados',
+//     exercises: 7
+//   }
+//   const part3 = {
+//     name: 'Estado de um componente',
+//     exercises: 14
+//   }
+
+//   return (
+//     <div>
+//       <Header course={course}/>
+//       <Content part1={part1} part2={part2} part3={part3}/>
+//       <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+//     </div>
+//   )
+
+// }
+
+// ----- Exercise 1.4 -----
+
 import Header from "./Header/Header";
 import Content from "./Content/Content";
 import Total from "./Total/Total";
 
 const App = () => {
   const course = 'Desenvolvimento de aplicação Half Stack'
-  const part1 = {
-    name: 'Fundamentos da biblioteca React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Usando props para passar dados',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'Estado de um componente',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentos da biblioteca React',
+      exercises: 10
+    },
+    {
+      name: 'Usando props para passar dados',
+      exercises: 7
+    },
+    {
+      name: 'Estado de um componente',
+      exercises: 14
+    }
+  ]
+
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3}/>
-      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
-  )
+  );
+};
 
-}
-export default App 
+
+export default App; 
