dc1afa507e92bf24b6c473d0393c98caac632ba7
Leonardo <129320373+leonardo-dos-santos@users.noreply.github.com>
2025-08-06 19:20:19 -0300

    Step1.7 adding the sum of the total clicks

--- part1/Exercises/unicafe_project_step1.6/src/App.js
+++ part1/Exercises/unicafe_project_step1.6/src/App.js
@@ -1,10 +1,33 @@
+// import { useState } from 'react'
+
+// const App = () => {
+//   const [good, setGood] = useState(0)
+//   const [neutral, setNeutral] = useState(0)
+//   const [bad, setBad] = useState(0)
+
+//   return (
+//     <div>
+//       <h1>Give Feedback</h1>
+//       <button onClick={() => setGood(good + 1)}>Good</button>   
+//       <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>   
+//       <button onClick={() => setBad(bad + 1)}>Bad</button>   
+
+//       <h2>Statistics</h2>
+//       <p>Good: {good}</p>
+//       <p>Neutral: {neutral}</p>
+//       <p>bad: {bad}</p>
+//     </div>
+//   )
+// }
+
 import { useState } from 'react'
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  let total = good + neutral + bad
+  
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -15,7 +38,8 @@ const App = () => {
       <h2>Statistics</h2>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
-      <p>bad: {bad}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {total}</p>
     </div>
   )
 }
