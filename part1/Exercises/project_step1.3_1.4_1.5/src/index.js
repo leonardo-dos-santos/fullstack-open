fb838ae729e817356cb20b0baa3fe8b4c0a240af
Leonardo <129320373+leonardo-dos-santos@users.noreply.github.com>
2025-08-06 19:14:25 -0300

    Step1.6 done

--- part1/Exercises/project_step1.3_1.4_1.5/src/index.js
+++ part1/Exercises/project_step1.3_1.4_1.5/src/index.js
@@ -1,4 +1,7 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import App from './App'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
---
+++ part1/Exercises/unicafe_project_step1.6/src/Header/Header.jsx
@@ -0,0 +1,6 @@
+const Header = () => {
+    return (
+      <h1>Give Feedback</h1>
+    )
+}
+export default Header;
