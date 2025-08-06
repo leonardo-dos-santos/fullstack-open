eb8c611c50d2234c1b6ef588ae4ccaf3b31baa23
Leonardo <129320373+leonardo-dos-santos@users.noreply.github.com>
2025-08-06 20:07:32 -0300

    Step1.14  done

--- part1/Exercises/unicafe_project_step1.6/src/App.js
+++ part1/Exercises/unicafe_project_step1.6/src/App.js
@@ -132,6 +132,48 @@
 //   )
 // }
 
+// Exercise 1.13
+
+// import { useState } from 'react'
+
+// const App = () => {
+//   const anecdotes = [
+//     'Se fazer algo dói, faça isso com mais frequência.',
+//     'Contratar mão de obra para um projeto de software que já está atrasado, faz com que se atrase mais ainda!',
+//     'Os primeiros 90% do código correspondem aos primeiros 10% do tempo de desenvolvimento... Os outros 10% do código correspondem aos outros 90% do tempo de desenvolvimento.',
+//     'Qualquer tolo escreve código que um computador consegue entender. Bons programadores escrevem código que humanos conseguem entender.',
+//     'Otimização prematura é a raiz de todo o mal.',
+//     'Antes de mais nada, depurar é duas vezes mais difícil do que escrever o código. Portanto, se você escrever o código da forma mais inteligente possível, você, por definição, não é inteligente o suficiente para depurá-lo.',
+//     'Programar sem o uso extremamente intenso do console.log é o mesmo que um médico se recusar a usar raio-x ou testes sanguíneos ao diagnosticar pacientes.',
+//     'A única maneira de ir rápido é ir bem.'
+//   ]
+
+//   const [selected, setSelected] = useState(0)
+//   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+//   const handleNext = () => {
+//     const randomIndex = Math.floor(Math.random() * anecdotes.length)
+//     setSelected(randomIndex)
+//   }
+
+//   const handleVote = () => {
+//     const copy = [...votes]
+//     copy[selected] += 1
+//     setVotes(copy)
+//   }
+
+//   return (
+//     <div>
+//       <h1>Anecdote of the day</h1>
+//       <p>{anecdotes[selected]}</p>
+//       <p>has {votes[selected]} vote(s)</p>
+
+//       <button onClick={handleVote}>Vote</button>
+//       <button onClick={handleNext}>Próxima anedota</button>
+//     </div>
+//   )
+// }
+
 import { useState } from 'react'
 
 const App = () => {
@@ -160,6 +202,9 @@ const App = () => {
     setVotes(copy)
   }
 
+  const maxVotes = Math.max(...votes)
+  const mostVotedIndex = votes.indexOf(maxVotes)
+
   return (
     <div>
       <h1>Anecdote of the day</h1>
@@ -168,6 +213,16 @@ const App = () => {
 
       <button onClick={handleVote}>Vote</button>
       <button onClick={handleNext}>Próxima anedota</button>
+
+      <h2>Anecdote with most votes</h2>
+      {maxVotes === 0 ? (
+        <p>Nenhuma anedota foi votada ainda.</p>
+      ) : (
+        <>
+          <p>{anecdotes[mostVotedIndex]}</p>
+          <p>com {maxVotes} voto(s)</p>
+        </>
+      )}
     </div>
   )
 }
