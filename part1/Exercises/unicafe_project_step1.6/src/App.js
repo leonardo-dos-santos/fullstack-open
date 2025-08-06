763071b0dd7bfaaa9a11bcc03d26e85b8d5ec474
Leonardo <129320373+leonardo-dos-santos@users.noreply.github.com>
2025-08-06 20:05:19 -0300

    Step1.13 done

--- part1/Exercises/unicafe_project_step1.6/src/App.js
+++ part1/Exercises/unicafe_project_step1.6/src/App.js
@@ -99,6 +99,39 @@
 //   )
 // }
 
+
+//Exercise 1.12
+
+// import { useState } from 'react'
+
+// const App = () => {
+//   const anecdotes = [
+//     'Se fazer algo dói, faça isso com mais frequência.',
+//     'Contratar mão de obra para um projeto de software que já está atrasado, faz com que se atrase mais ainda!',
+//     'Os primeiros 90% do código correspondem aos primeiros 10% do tempo de desenvolvimento... Os outros 10% do código correspondem aos outros 90% do tempo de desenvolvimento.',
+//     'Qualquer tolo escreve código que um computador consegue entender. Bons programadores escrevem código que humanos conseguem entender.',
+//     'Otimização prematura é a raiz de todo o mal.',
+//     'Antes de mais nada, depurar é duas vezes mais difícil do que escrever o código. Portanto, se você escrever o código da forma mais inteligente possível, você, por definição, não é inteligente o suficiente para depurá-lo.',
+//     'Programar sem o uso extremamente intenso do console.log é o mesmo que um médico se recusar a usar raio-x ou testes sanguíneos ao diagnosticar pacientes.',
+//     'A única maneira de ir rápido é ir bem.'
+//   ]
+
+//   const [selected, setSelected] = useState(0)
+
+//   const handleNext = () => {
+//     const randomIndex = Math.floor(Math.random() * anecdotes.length)
+//     setSelected(randomIndex)
+//   }
+
+//   return (
+//     <div>
+//       <h1>Anecdote of the day</h1>
+//       <p>{anecdotes[selected]}</p>
+//       <button onClick={handleNext}>Próxima anedota</button>
+//     </div>
+//   )
+// }
+
 import { useState } from 'react'
 
 const App = () => {
@@ -114,16 +147,26 @@ const App = () => {
   ]
 
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
   const handleNext = () => {
     const randomIndex = Math.floor(Math.random() * anecdotes.length)
     setSelected(randomIndex)
   }
 
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
   return (
     <div>
       <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} vote(s)</p>
+
+      <button onClick={handleVote}>Vote</button>
       <button onClick={handleNext}>Próxima anedota</button>
     </div>
   )
