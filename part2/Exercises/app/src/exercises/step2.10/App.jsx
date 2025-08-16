266bd7a4a6413f059ef5feeafbee21c1bec649c8
Leonardo <129320373+leonardo-dos-santos@users.noreply.github.com>
2025-08-16 13:11:39 -0300

    Step 2.10 - Creating a phonebook, componentization

---
+++ part2/Exercises/app/src/exercises/step2.10/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import Filter from './Filter'
+import PersonForm from './PersonForm'
+import persons from './Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas 040-1234567', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    
+    const nameExists = persons.some(person => person.name === newName)
+
+    if (nameExists) {
+      alert(`${newName} is already added to phonebook`)
+    } else {
+      const personObject = { 
+        name: newName,
+        number: newNumber,
+        id: persons.length + 1
+      }
+      setPersons(persons.concat(personObject))
+    }
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      
+      <Filter filter={filter} handleFilterChange={handleFilterChange} />
+
+      <h2>Add a new</h2>
+      
+      <PersonForm 
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      
+      <h2>Numbers</h2>
+        <Persons personsToShow={personsToShow} />
+    </div>
+  )
+}
+
+export default App
---
+++ part2/Exercises/app/src/exercises/step2.10/Persons.jsx
@@ -0,0 +1,13 @@
+const Person = ({ person }) => (
+  <li>{person.name} — {person.number}</li>
+)
+
+const Persons = ({ personsToShow }) => (
+  <ul>
+    {personsToShow.map(person => 
+      <Person key={person.id} person={person} />
+    )}
+  </ul>
+)
+
+export default { Person, Persons }
